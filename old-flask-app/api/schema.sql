02fcbdb55dbaf534418300ddbe33b40d1605d040
FranVi <fvidalmateo@gmail.com>
2026-01-19 16:43:08 +0100

    Clean up: Remove old Flask files from main branch

--- old-flask-app/api/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS transaction_history;
-
-CREATE TABLE transaction_history (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  timestamp TEXT NOT NULL,
-  total REAL NOT NULL,
-  received REAL NOT NULL,
-  change REAL NOT NULL
-);
--- old-flask-app/compile_translations.py
+++
@@ -1,4 +0,0 @@
-from babel.messages.frontend import CommandLineInterface
-
-if __name__ == '__main__':
-    CommandLineInterface().run(['pybabel', 'compile', '-d', 'api/translations'])
